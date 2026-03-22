686226244fd76eaa3f384b900958da2ed7d0b440
Arsen <arsenyo198510@gmail.com>
2026-03-22 23:16:44 +0200

    Common added.

---
+++ src/BookingService.Domain/Common/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Common
+{
+    public record Error(string Code, string Description)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+    }
+}
---
+++ src/BookingService.Domain/Common/Result.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Common
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public Error Error { get; }
+        public T? Value { get; }
+
+        public Result(bool isSuccess, Error error, T? value)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+            Value = value;
+        }
+
+        public static Result<T> Success(T value) => new Result<T>(true, Error.None, value);
+
+        public static Result<T> Failure(Error error) => new Result<T>(false, error, default);
+    }
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public Error Error { get; }
+
+        private Result(bool isSuccess, Error error)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Success() => new(true, Error.None);
+        public static Result Failure(Error error) => new(false, error);
+    }
+}
