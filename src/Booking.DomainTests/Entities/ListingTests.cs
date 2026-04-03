85cd6ac25e661c6b43967a58014f1962c60733ab
Arsen <arsenyo198510@gmail.com>
2026-04-03 22:49:13 +0300

    Start work with application layer. Feat: Created interfaces for Command/CommandHandler and Query/QueryHandler

---
+++ src/Booking.DomainTests/Entities/ListingTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Booking.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Entities.Tests
+{
+    [TestClass()]
+    public class ListingTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddAmenityTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/BookingService.Application/Abstractions/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using Booking.Domain.Common;
+using MediatR;
+
+namespace Booking.Application.Abstractions
+{
+    public interface ICommand : IRequest<Result> { }
+
+    public interface ICommand<TResponse> : IRequest<Result<TResponse>> { }
+}
