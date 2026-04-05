9b024e24649265bb548055634f492707014279b4
Arsen <arsenyo198510@gmail.com>
2026-04-05 20:17:56 +0300

    Created Booking controllers

---
+++ src/BookingService.API/Controllers/BookingControllers.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Booking.Application.UseCases;
+using Booking.Application.UseCases.Bookings.GetAllBookings;
+using Booking.Domain.Common;
+
+namespace Booking.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/bookings")]
+    public class BookingControllers : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public BookingControllers(ISender sender)
+        {
+            _sender = sender;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(CancellationToken ct)
+        {
+            var result = await _sender.Send(new GetAllBookingsQuery(), ct);
+
+            return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
+        }
+
+    }
+}
