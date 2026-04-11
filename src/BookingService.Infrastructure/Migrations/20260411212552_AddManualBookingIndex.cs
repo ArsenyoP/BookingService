b69813050cf41d1fedbcddc3bd0bd0e49bd40f8d
Arsen <arsenyo198510@gmail.com>
2026-04-12 00:27:49 +0300

    Feat: manually added filtering index for Bookings

---
+++ src/BookingService.Infrastructure/Migrations/20260411212552_AddManualBookingIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddManualBookingIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookingss_Status_StartDate",
+                table: "Bookingss",
+                columns: new[] { "Status", "StartDate" },
+                descending: new[] { false, true });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Bookingss_Status_StartDate",
+                table: "Bookingss");
+        }
+    }
+}
