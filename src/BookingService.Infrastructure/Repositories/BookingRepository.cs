a7e375869009ad9da3c94e4fb40121e5e76e5f7e
Arsen <arsenyo198510@gmail.com>
2026-04-18 14:03:13 +0300

    Refactor: Moved  IsRoomAvailable method from query to repository and rewrited to EF Core instead of Dapper

--- src/BookingService.Infrastructure/Repositories/BookingRepository.cs
+++ src/BookingService.Infrastructure/Repositories/BookingRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Booking.Domain.Entities;
 using Booking.Domain.Interfaces.IRepositories;
 using Booking.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Booking.Domain.Enums;
 
 namespace Booking.Infrastructure.Repositories
 {
@@ -16,6 +18,20 @@ namespace Booking.Infrastructure.Repositories
         {
             ArgumentNullException.ThrowIfNull(booking);
             _dbContext.Bookings.Remove(booking);
+        }
+
+        public async Task<bool> IsRoomAvailableAsync(Guid roomId, DateOnly start, DateOnly end, CancellationToken ct = default)
+        {
+            var result = await _dbContext.Bookings.AnyAsync(x =>
+                x.RoomId == roomId &&
+                x.Status != BookingStatus.Cancelled &&
+                x.Status != BookingStatus.Expired &&
+                x.Period.StartDate < end &&
+                x.Period.EndDate > start,
+                ct
+                );
+
+            return result;
         }
     }
 }
