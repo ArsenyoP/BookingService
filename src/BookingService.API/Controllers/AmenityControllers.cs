e7ed011c3e31474ba5891ab8f89cff49928a52b9
ArsenyoP <arsenyo198510@gmail.com>
2026-04-19 21:08:49 +0300

    Feat: Added RemoveAmenityFromRoom method

--- src/BookingService.API/Controllers/AmenityControllers.cs
+++ src/BookingService.API/Controllers/AmenityControllers.cs
@@ -1,6 +1,7 @@
 ﻿using Booking.Application.UseCases.Amenities;
 using Booking.Application.UseCases.Amenities.AddAmenityToListing;
 using Booking.Application.UseCases.Amenities.AddAmenityToRoom;
+using Booking.Application.UseCases.Amenities.RemoveAmenityFromRoom;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +24,16 @@ namespace Booking.API.Controllers
 
         [HttpPost("addToRoom")]
         public async Task<IActionResult> AddAmenityToRoom([FromBody] AddAmenityToRoomCommand command, CancellationToken ct)
+        {
+            var result = await _sender.Send(command, ct);
+
+            return result.IsSuccess
+                ? Ok(result.Value)
+                : BadRequest(result.Error);
+        }
+
+        [HttpPost("removeFromRoom")]
+        public async Task<IActionResult> RemoveAmenityFromRoom([FromBody] RemoveAmenityFromRoomCommand command, CancellationToken ct)
         {
             var result = await _sender.Send(command, ct);
 
