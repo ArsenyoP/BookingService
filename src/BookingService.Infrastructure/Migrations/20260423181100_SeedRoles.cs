7bbc656d7687e5740ffdb85f19fe4285c2519d5d
ArsenyoP <arsenyo198510@gmail.com>
2026-04-23 21:25:34 +0300

    Feat: Added roles seeding to database

---
+++ src/BookingService.Infrastructure/Migrations/20260423181100_SeedRoles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Booking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("3474648c-112d-460c-bc8a-61be046b3078"), null, "Admin", "ADMIN" },
+                    { new Guid("a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5d"), null, "Guest", "GUEST" },
+                    { new Guid("eb4b3260-05fb-470c-af9f-b9e9b9bcd85e"), null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("3474648c-112d-460c-bc8a-61be046b3078"));
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5d"));
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("eb4b3260-05fb-470c-af9f-b9e9b9bcd85e"));
+        }
+    }
+}
