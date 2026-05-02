f99d3da9ae26d1263e48fdf11f4a9210440bec54
ArsenyoP <arsenyo198510@gmail.com>
2026-05-02 23:33:16 +0300

    Feat: Implemented IExceptionHandler for flobal exception handling

---
+++ src/BookingService.API/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.API.Middleware
+{
+    public sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> _logger) : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            var (statusCode, title) = exception switch
+            {
+                ValidationException => (StatusCodes.Status400BadRequest, "Validation Error"),
+                KeyNotFoundException => (StatusCodes.Status404NotFound, "Resource Not Found"),
+                UnauthorizedAccessException => (StatusCodes.Status401Unauthorized, "Unauthorized"),
+                _ => (StatusCodes.Status500InternalServerError, "Server Error")
+            };
+
+            if (statusCode == StatusCodes.Status500InternalServerError)
+                _logger.LogError(exception, "Unhandled exception: {Message}", exception.Message);
+            else
+                _logger.LogWarning("Request error: {Message}", exception.Message);
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = statusCode,
+                Title = title,
+                Detail = exception.Message,
+                Instance = httpContext.Request.Path
+            };
+
+
+            if (exception is ValidationException validationException)
+            {
+                var validationErrors = validationException.Errors
+                    .GroupBy(e => e.PropertyName)
+                    .ToDictionary(g => g.Key, g => g.Select(e => e.ErrorMessage).ToArray());
+
+                var validationProblem = new ValidationProblemDetails(validationErrors)
+                {
+                    Status = StatusCodes.Status400BadRequest,
+                    Title = "Validation Error",
+                    Detail = "See the errors field for details."
+                };
+
+                await httpContext.Response.WriteAsJsonAsync(validationProblem, cancellationToken);
+                return true;
+            }
+
+
+            httpContext.Response.StatusCode = statusCode;
+
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+            
+            return true;
+        }
+    }
+}
