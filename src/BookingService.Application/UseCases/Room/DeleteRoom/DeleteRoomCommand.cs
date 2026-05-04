61630c4ab9530ecb6e89e0bba464bc4bd0a3606d
ArsenyoP <arsenyo198510@gmail.com>
2026-05-05 00:09:03 +0300

    Feat: Setted up caching and invalidation for Room

--- src/BookingService.Application/UseCases/Room/DeleteRoom/DeleteRoomCommand.cs
+++ src/BookingService.Application/UseCases/Room/DeleteRoom/DeleteRoomCommand.cs
@@ -2,5 +2,8 @@
 
 namespace Booking.Application.UseCases.Room.DeleteRoom
 {
-    public sealed record DeleteRoomCommand(Guid roomId) : ICommand<Guid>;
+    public sealed record DeleteRoomCommand(Guid roomId) : ICommand<Guid>, ICacheInvalidationCommand
+    {
+        public string Key => $"room:{roomId}";
+    }
 }
--- src/BookingService.Application/UseCases/Room/GetById/GetByIdQuery.cs
+++ src/BookingService.Application/UseCases/Room/GetById/GetByIdQuery.cs
@@ -4,5 +4,10 @@ using Booking.Application.DTOs.Rooms;
 
 namespace Booking.Application.UseCases.Room.GetById
 {
-    public sealed record GetByIdQuery(Guid Id) : IQuery<RoomResponseDto>;
+    public sealed record GetByIdQuery(Guid Id) : IQuery<RoomResponseDto>, ICachableQuery
+    {
+        public string Key => $"room:{Id}";
+
+        public TimeSpan Expiration => TimeSpan.FromMinutes(5);
+    }
 }
