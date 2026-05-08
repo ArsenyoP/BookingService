40fbe19fd4899454a60c1e30d73071bb3e0b6629
ArsenyoP <arsenyo198510@gmail.com>
2026-05-08 21:48:06 +0300

    Feat: Created RoomQueryObject

---
+++ src/BookingService.Application/Helpers/Room/RoomQueryObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Booking.Application.Helpers.Room
+{
+    public sealed record RoomQueryObject(
+        decimal? MinPrice = null,
+        decimal? MaxPrice = null,
+        int? MinAdults = null,
+        int? MinChildren = null,
+        string Type = "",
+        List<string>? AmenityNames = null,
+        int Page = 1,
+        int PageSize = 10
+        );
+}
