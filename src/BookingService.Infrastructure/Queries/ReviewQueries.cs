9c4417d2a231c69cbad44b681162905185f14f10
ArsenyoP <arsenyo198510@gmail.com>
2026-05-10 21:45:17 +0300

    Feat: Implemented GetByUserId

--- src/BookingService.Infrastructure/Queries/ReviewQueries.cs
+++ src/BookingService.Infrastructure/Queries/ReviewQueries.cs
@@ -75,9 +75,35 @@ namespace Booking.Infrastructure.Queries
             return result.ToList().AsReadOnly();
         }
 
-        public Task<IReadOnlyList<ReviewResponseDto>> GetByUserId(int page, int pageSize, Guid userId, CancellationToken ct)
+        public async Task<IReadOnlyList<ReviewResponseDto>> GetByUserId(int page, int pageSize, Guid userId, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            var connection = new SqlConnection(connectionString);
+
+            var sql = @"SELECT 
+	                r.Score,
+	                r.Text,
+	                u.UserName,
+	                r.TargetType,
+	                ISNULL(l.Title, rm.Title) AS TargetTitle,
+	                r.CreatedAt,
+	                r.IsEdited	
+                FROM Reviews r
+                INNER JOIN Users u ON u.Id = r.UserId
+                LEFT JOIN Listings l ON l.Id = r.TargetId AND r.TargetType = 'Listing'
+                LEFT JOIN Rooms rm ON rm.Id = r.TargetId AND r.TargetType = 'Room'
+                WHERE u.Id = @UserId
+                ORDER BY r.CreatedAt DESC
+                OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY";
+
+            var offset = (page - 1) * pageSize;
+
+            var command = new CommandDefinition(
+                sql,
+                new { UserId = userId, Offset = offset, PageSize = pageSize },
+                cancellationToken: ct);
+
+            var result = await connection.QueryAsync<ReviewResponseDto>(command);
+            return result.ToList().AsReadOnly();
         }
     }
 }
