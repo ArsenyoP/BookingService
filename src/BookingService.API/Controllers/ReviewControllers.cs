fc874af0b681c9ddfd914d05b742692027bbfc62
ArsenyoP <arsenyo198510@gmail.com>
2026-05-11 12:24:40 +0300

    Feat: Implemented GetAllReviews.

---
+++ src/BookingService.API/Controllers/ReviewControllers.cs
@@ -0,0 +1,24 @@
+﻿using Booking.Application.UseCases.Reviews.GetAllReviews;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace Booking.API.Controllers
+{
+    [ApiController]
+    [EnableRateLimiting("fixed")]
+    [Route("api/reviews")]
+    public class ReviewControllers(ISender _sender) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] int page = 1, [FromQuery] int pageSize = 10, CancellationToken ct = default)
+        {
+            var command = new GetAllReviewsQuery(page, pageSize);
+
+            var result = await _sender.Send(command, ct);
+            return result.IsSuccess
+                ? Ok(result.Value)
+                : BadRequest(result.Error);
+        }
+    }
+}
---
+++ src/BookingService.Application/UseCases/Reviews/GetAllReviews/GetAllReviewsQuery.cs
@@ -0,0 +1,7 @@
+﻿using Booking.Application.Abstractions;
+using Booking.Application.DTOs.Reviews;
+
+namespace Booking.Application.UseCases.Reviews.GetAllReviews
+{
+    public record GetAllReviewsQuery(int page, int pageSize) : IQuery<IReadOnlyList<ReviewResponseDto>>;
+}
