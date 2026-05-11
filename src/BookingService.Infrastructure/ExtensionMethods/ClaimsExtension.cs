021ae7335707523ce5e47ddbbf213fa275103d9b
ArsenyoP <arsenyo198510@gmail.com>
2026-05-11 22:27:42 +0300

    Feat: Created extension method GetUserID for extracting Id of current session from claims

---
+++ src/BookingService.Infrastructure/ExtensionMethods/ClaimsExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace Booking.Infrastructure.ExtensionMethods
+{
+    public static class ClaimsExtension
+    {
+        public static string GetUserID(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier)
+                   ?? user.FindFirstValue("sub")!;
+        }
+    }
+}
