f85e17c5b41703387f7bdffd02d4bbb79aa13570
ArsenyoP <arsenyo198510@gmail.com>
2026-05-12 11:22:11 +0300

    Feat: Added methods for UnitOfWork: BeginTransactionAsync, CommitAsync, RollbackAsync

--- src/BookingService.Application/Interfaces/IUnitOfWork.cs
+++ src/BookingService.Application/Interfaces/IUnitOfWork.cs
@@ -1,8 +1,19 @@
-﻿namespace Booking.Application.Interfaces
+﻿using System.Data;
+
+namespace Booking.Application.Interfaces
 {
     public interface IUnitOfWork
     {
         public Task SaveChangesAsync(CancellationToken ct = default);
+
+        Task<IDisposable> BeginTransactionAsync(CancellationToken ct = default);
+
+        Task CommitAsync(CancellationToken ct = default);
+
+        Task RollbackAsync(CancellationToken ct = default);
+
+        IDbTransaction? GetCurrentTransaction();
+
         Task<T> ExecuteInSerializableTransactionAsync<T>(
             Func<Task<T>> operation,
             CancellationToken ct = default);
