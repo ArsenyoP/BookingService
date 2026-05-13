eb61e049d01e5eeadec0bdb0493cc96631480ebf
ArsenyoP <arsenyo198510@gmail.com>
2026-05-13 23:18:42 +0300

    Feat: Created mapepr for Review which maps to ReviewResponseDto

---
+++ src/BookingService.Application/Mappers/ReviewMappers.cs
@@ -0,0 +1,22 @@
+﻿using Booking.Application.DTOs.Reviews;
+using Booking.Domain.Entities;
+
+namespace Booking.Application.Mappers
+{
+    public static class ReviewMappers
+    {
+        public static ReviewResponseDto MapFromReviewToResponseDto(this Review review, string targetTitle, string userName)
+        {
+            return new ReviewResponseDto
+            {
+                CreatedAt = review.CreatedAt,
+                IsEdited = review.IsEdited,
+                Score = review.Score,
+                TargetTitle = targetTitle,
+                TargetType = review.TargetType.ToString(),
+                Text = review.Text,
+                UserName = userName
+            };
+        }
+    }
+}
