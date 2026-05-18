7b79c3d06390faec66f990b58e88b9011805a427
ArsenyoP <arsenyo198510@gmail.com>
2026-05-18 23:11:18 +0300

    Feat: Set up OutboxMessage model and DB table

---
+++ src/BookingService.Infrastructure/Data/EntitiesConfigurations/OutboxMessageConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Booking.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Booking.Infrastructure.Data.EntitiesConfigurations
+{
+    public sealed class OutboxMessageConfiguration : IEntityTypeConfiguration<OutboxMessage>
+    {
+        public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+        {
+            builder.ToTable("OutboxMessages");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Type)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(x => x.Content)
+                .HasColumnType("nvarchar(max)")
+                .IsRequired();
+
+            builder.Property(x => x.OccurredOnUtc)
+                .IsRequired();
+
+            builder.Property(x => x.ProcessedOnUtc)
+                .IsRequired(false);
+
+            builder.Property(x => x.Error)
+                .HasColumnType("nvarchar(max)")
+                .IsRequired(false);
+
+            builder.HasIndex(x => new { x.ProcessedOnUtc, x.OccurredOnUtc })
+                .HasDatabaseName("IX_OutboxMessages_ProcessedOnUtc_OccurredOnUtc");
+        }
+    }
+}
