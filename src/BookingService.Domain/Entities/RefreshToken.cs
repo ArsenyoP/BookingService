0dd97ef22b7bb1a49fd3969349e0c51f9322ce4c
ArsenyoP <arsenyo198510@gmail.com>
2026-05-22 22:03:27 +0300

    Feat: Created RefreshToken entity. Added RefreshToken to DB

---
+++ src/BookingService.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿using Booking.Domain.Common;
+
+namespace Booking.Domain.Entities
+{
+    public class RefreshToken : Entity
+    {
+        public string Token { get; private set; }
+        public Guid UserId { get; private set; }
+        public DateTime ExpiresOnUtc { get; private set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ src/BookingService.Infrastructure/Data/EntitiesConfigurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Booking.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Booking.Infrastructure.Data.EntitiesConfigurations
+{
+    internal class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.ToTable("RefreshTokens");
+
+            builder.Property(x => x.Token)
+                .HasMaxLength(32)
+                .IsFixedLength(true);
+
+            builder.HasOne(r => r.User).WithMany().HasForeignKey(r => r.UserId);
+
+            builder.HasIndex(x => x.Token).IsUnique();
+        }
+    }
+}
