bbebbb03a4825c24a989fa185b3262cd5791210f
ArsenyoP <arsenyo198510@gmail.com>
2026-05-29 19:05:37 +0300

    Feat: Created DTOs for OpenWeather

--- src/BookingService.Application/DTOs/OpenWeather/OpenWeatherDto.cs
+++ src/BookingService.Application/DTOs/OpenWeather/OpenWeatherDto.cs
@@ -1,4 +1,61 @@
-﻿namespace Booking.Application.DTOs.OpenWeather
+﻿using System.Text.Json.Serialization;
+
+namespace Booking.Application.DTOs.OpenWeather
 {
-    public sealed record OpenWeatherDto();
+    public record OpenWeatherResponse
+    {
+        [JsonPropertyName("name")]
+        public string CityName { get; init; } = string.Empty;
+
+        [JsonPropertyName("weather")]
+        public List<WeatherDescriptionData> Weather { get; init; } = [];
+
+        [JsonPropertyName("main")]
+        public MainData Main { get; init; } = new();
+
+        [JsonPropertyName("wind")]
+        public WindData Wind { get; init; } = new();
+
+        [JsonPropertyName("clouds")]
+        public CloudsData Clouds { get; init; } = new();
+    }
+
+    public record WeatherDescriptionData
+    {
+        [JsonPropertyName("main")]
+        public string Main { get; init; } = string.Empty;
+
+        [JsonPropertyName("description")]
+        public string Description { get; init; } = string.Empty;
+    }
+
+    public record MainData
+    {
+        [JsonPropertyName("temp")]
+        public decimal Temp { get; init; }
+    }
+
+    public record WindData
+    {
+        [JsonPropertyName("speed")]
+        public decimal Speed { get; init; }
+    }
+
+    public record CloudsData
+    {
+        [JsonPropertyName("all")]
+        public int All { get; init; }
+    }
+
+
+
+    public record WeatherResultDto
+    {
+        public string CityName { get; init; } = string.Empty;
+        public decimal Temperature { get; init; }
+        public int CloudinessPercent { get; init; }
+        public decimal WindSpeed { get; init; }
+        public string Main { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+    }
 }
--- src/BookingService.Application/DTOs/OpenWeather/OpenWeatherGeoDto.cs
+++ src/BookingService.Application/DTOs/OpenWeather/OpenWeatherGeoDto.cs
@@ -1,4 +1,16 @@
-﻿namespace Booking.Application.DTOs.OpenWeather
+﻿using System.Text.Json.Serialization;
+
+namespace Booking.Application.DTOs.OpenWeather
 {
-    public sealed record OpenWeatherGeoDto();
+    public sealed record OpenWeatherGeoDto
+    {
+        [JsonPropertyName("lon")]
+        public double Longitude { get; set; }
+
+        [JsonPropertyName("lat")]
+        public double Latitude { get; set; }
+
+        [JsonPropertyName("name")]
+        public string CityName { get; set; } = string.Empty;
+    }
 }
