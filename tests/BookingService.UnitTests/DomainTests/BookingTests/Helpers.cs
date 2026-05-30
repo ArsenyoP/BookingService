64e6c15311a1cd6a99dfc84e987bd5d43867b5c1
ArsenyoP <arsenyo198510@gmail.com>
2026-05-30 07:24:36 +0300

    Feat: Created helper methods for Booking testing: CreateRoom, CreateUser

---
+++ tests/BookingService.UnitTests/DomainTests/BookingTests/Helpers.cs
@@ -0,0 +1,38 @@
+﻿using Booking.Domain.Entities;
+using Booking.Domain.Enums;
+
+namespace BookingService.UnitTests.DomainTests.BookingTests
+{
+    public class Helpers
+    {
+        public User CreateUser()
+        {
+            DateOnly birthdayDate = new DateOnly(2, 10, 2000);
+
+            var userResult = User.Create(
+                "TestFirstName",
+                "TestLastName",
+                birthdayDate,
+                "testemail@gmail.com",
+                "TestUsername");
+
+            return userResult.Value!;
+        }
+
+        public Room CreateRoom()
+        {
+            Guid listingId = Guid.Parse("c0de5ddc-6db8-4fcd-a2ff-f60dc446d1ed");
+
+            var roomResult = Room.Create(
+                "TestRoomTitle",
+                "Test room description",
+                RoomType.Standard,
+                300,
+                3,
+                2,
+                listingId);
+
+            return roomResult.Value!;
+        }
+    }
+}
