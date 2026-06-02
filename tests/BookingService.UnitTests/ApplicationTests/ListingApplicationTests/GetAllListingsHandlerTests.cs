678d9ae566bb51f4c30c10e32ddf46acba60c447
ArsenyoP <arsenyo198510@gmail.com>
2026-06-02 11:53:37 +0300

    Feat: Created GetAllListingsHandlerTests

---
+++ tests/BookingService.UnitTests/ApplicationTests/ListingApplicationTests/GetAllListingsHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using Booking.Application.DTOs.Listings;
+using Booking.Application.Helpers.Room;
+using Booking.Application.Interfaces.IQueries;
+using Booking.Application.UseCases.Listing.GetAllListings;
+using FluentAssertions;
+using Moq;
+
+namespace BookingService.UnitTests.ApplicationTests.ListingApplicationTests
+{
+    public class GetAllListingsHandlerTests
+    {
+        private readonly Mock<IListingQueries> _listingQueriesMock;
+        private readonly GetAllListingsHandler _handler;
+
+        public GetAllListingsHandlerTests()
+        {
+            _listingQueriesMock = new Mock<IListingQueries>();
+            _handler = new GetAllListingsHandler(_listingQueriesMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ValidQuery_ReturnsSuccessWithListings()
+        {
+            var queryObject = new ListingQueryObject(
+                Title: "Apartment",
+                PageSize: 10,
+                Page: 1
+            );
+            var query = new GetAllListingsQuery(queryObject);
+
+            IReadOnlyList<ListingResponseDto> expectedListings = new List<ListingResponseDto>();
+
+            _listingQueriesMock
+                .Setup(x => x.GetAllPagedAsync(query.QueryObject, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(expectedListings);
+
+            var result = await _handler.Handle(query, It.IsAny<CancellationToken>());
+
+            result.IsSuccess.Should().Be(true);
+            result.Value.Should().BeEquivalentTo(expectedListings);
+            _listingQueriesMock.Verify(x => x.GetAllPagedAsync(query.QueryObject, It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
