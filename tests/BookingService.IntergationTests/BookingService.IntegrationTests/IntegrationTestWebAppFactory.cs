a3396931906621b12157cd211687bb1a37ff8d31
ArsenyoP <arsenyo198510@gmail.com>
2026-06-03 13:30:30 +0300

    Feat: Configured WebAppFactory for integration tests

---
+++ tests/BookingService.IntergationTests/BookingService.IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,45 @@
+﻿using Booking.API;
+using Booking.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.MsSql;
+
+namespace BookingService.IntegrationTests
+{
+    public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>
+    {
+        private readonly MsSqlContainer _dbContainer = new MsSqlBuilder("mcr.microsoft.com/mssql/server:2022-latest")
+            .Build();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d =>
+                    d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseSqlServer(_dbContainer.GetConnectionString());
+                });
+            });
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _dbContainer.StartAsync();
+        }
+        public new async Task DisposeAsync()
+        {
+            await _dbContainer.StopAsync();
+        }
+    }
+}
