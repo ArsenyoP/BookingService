87ce95abdb5b084eb38ccda57c2365cbf132257c
ArsenyoP <arsenyo198510@gmail.com>
2026-06-10 00:40:11 +0300

    /

---
+++ src/BookingService.Domain/Interfaces/Services/IEmbaddingService.cs
@@ -0,0 +1,10 @@
+﻿using Booking.Domain.DomainEvents;
+
+
+namespace Booking.Domain.Interfaces.Services
+{
+    public interface IEmbaddingService
+    {
+        Task EmbaddeEvent(RoomCreatedDomainEvent domainEvent, CancellationToken ct = default);
+    }
+}
