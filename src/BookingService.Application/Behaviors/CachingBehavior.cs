7beadc8e08bb49ece8a92482ab74992ae4b2525c
ArsenyoP <arsenyo198510@gmail.com>
2026-09-13 17:08:03 +0300

    Added logging to CachingBehavior

--- src/BookingService.Application/Behaviors/CachingBehavior.cs
+++ src/BookingService.Application/Behaviors/CachingBehavior.cs
@@ -2,10 +2,12 @@
 using Booking.Application.Interfaces.Services;
 using Booking.Domain.Common;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Booking.Application.Behaviors
 {
-    public sealed class CachingBehavior<TRequest, TResponse>(ICacheService _cache)
+    public sealed class CachingBehavior<TRequest, TResponse>(ICacheService _cache,
+        ILogger<CachingBehavior<TRequest, TResponse>> _logger)
     : IPipelineBehavior<TRequest, Result<TResponse>>
         where TRequest : IRequest<Result<TResponse>>, ICachableQuery
     {
@@ -17,16 +19,30 @@ namespace Booking.Application.Behaviors
             var cached = await _cache.GetAsync<TResponse>(cacheable.Key, cancellationToken);
 
             if (cached is not null)
+            {
+                _logger.LogInformation($"Returned from cache with key: {cacheable.Key}, data: {cached}");
                 return Result<TResponse>.Success(cached);
+            }
 
             var response = await next();
 
             if (response.Value is null)
                 return response;
 
-            await _cache.SetAsync<TResponse>(cacheable.Key,
-                response.Value, cacheable.Expiration, cancellationToken);
+            try
+            {
+                await _cache.SetAsync<TResponse>(
+                    cacheable.Key,
+                    response.Value,
+                    cacheable.Expiration,
+                    cancellationToken);
 
+                _logger.LogInformation("Successfully cached value with key: {Key}", request.Key);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to set cache for key: {Key}", request.Key);
+            }
 
             return response;
         }
