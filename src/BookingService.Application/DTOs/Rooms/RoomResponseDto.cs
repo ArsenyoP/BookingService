15376705948849307053536a9ebfd69651d0f586
ArsenyoP <arsenyo198510@gmail.com>
2026-09-20 01:35:07 +0300

    Added to RoomResponseDto fields for adress

--- src/BookingService.Application/DTOs/Rooms/RoomResponseDto.cs
+++ src/BookingService.Application/DTOs/Rooms/RoomResponseDto.cs
@@ -13,6 +13,10 @@ namespace Booking.Application.DTOs.Rooms
         public int AdultsCapacity { get; init; }
         public int ChildrenCapacity { get; init; }
         public string ListingTitle { get; init; }
+        public string Country { get; init; } = "Unknown";
+        public string City { get; init; } = "Unknown";
+        public string Street { get; init; } = "Unknown";
+        public string HouseNumbe { get; init; } = "Unknown";
         public Guid ListingId { get; init; }
         public decimal AverageRating { get; init; }
         public int ReviewsCount { get; set; }
